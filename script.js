5753da03f4df424b118435e0ec7f127ddeb26011
Manuel Moran <manumoran158@gmail.com>
2023-02-01 11:55:58 -0300

    cartas de descripciones 1.0

--- script.js
+++ script.js
@@ -29,3 +29,23 @@ function mostrarHogar(){
     document.getElementById('3-1').style.backgroundImage = "url(images/rombo.png)";
     
 }
+
+function mostrar1(){
+//Selecciono todos los elementos con la class 'clase'
+var Elementos = document.getElementsByClassName('img-empresa');
+  
+//Recorro el array obtenido
+[].forEach.call(Elementos, function (item) {
+  //Cambio el color del div por fines demostrativos.
+  item.style.display = "none";
+}); 
+
+//Selecciono todos los elementos con la class 'clase'
+var Elementos1 = document.getElementsByClassName('empresa1');
+  
+//Recorro el array obtenido
+[].forEach.call(Elementos1, function (item) {
+  //Cambio el color del div por fines demostrativos.
+  item.style.display = "flex";
+}); 
+}
